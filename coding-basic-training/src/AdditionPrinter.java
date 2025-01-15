c23c488293a3f7b1f36478215b7a189c003d8a93
sangunlee6 <sangunlee6@gmail.com>
2025-01-15 11:30:33 +0900

    덧셈식 출력하기

---
+++ coding-basic-training/src/AdditionPrinter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AdditionPrinter {
+    /**
+     * 덧셈식 출력하기
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
